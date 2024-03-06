32b7465092edbe5823d2b8ca70d26fe4adbc26bb
Christina Sirgunas <n10212477@qut.edu.au>
2024-03-06 14:23:38 +1000

    Authenticaion class implemented

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
     private static ArrayList<User> users = new ArrayList<>();
 
     // Mock authentication service that always returns the first user when log in, and does nothing when sign up
-    private static IAuthenticationService authService = new IAuthenticationService() {
+    private static IAuthenticationService authService = new AuthenticationService(users) {
         @Override
         public User signUp(String username, String password) {
             return null;
@@ -91,6 +91,12 @@ public class Main {
         String password = scanner.nextLine();
         User user = authService.signUp(username, password);
         // TODO Later: Shows a message based on the result
+        if (user != null){
+            System.out.print("User " + username + " has been created successfully!");
+        }
+        else{
+            System.out.print("The username is already taken!");
+        }
     }
 
     /**
